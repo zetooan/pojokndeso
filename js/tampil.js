9ae60210ac81ef3a69d9ff5273ece0f266a35d40
zeto <43518064+zetooan@users.noreply.github.com>
2021-10-25 10:29:23 +0700

    update tata letak
    wave, wisata populer, dan berita terkini beranda

---
+++ js/tampil.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	function slideberanda(){
+		o = 4;
+		function button_slide(i){
+			i = i-1;
+			slide ='<button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
+			while()
+		}
+		
+		$('#btn_slide_beranda').hidden();
+
+
+
+		slideberanda = "";
+	}
+
+});
